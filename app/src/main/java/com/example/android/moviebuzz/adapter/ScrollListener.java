4fe23b3be1bfcc1624afa31d6769034c8d1a2290
Purushotham Reddy Cheerla <purushothamcheerla@gmail.com>
2017-12-11 00:18:48 -0500

    remove unused files using lint run

---
+++ app/src/main/java/com/example/android/moviebuzz/adapter/ScrollListener.java
@@ -0,0 +1,10 @@
+package com.example.android.moviebuzz.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by purus on 12/10/2017.
+ */
+
+public class ScrollListener extends RecyclerView.OnScrollListener {
+}
--- app/src/main/java/com/example/android/moviebuzz/fragments/MovieFragment.java
+++ app/src/main/java/com/example/android/moviebuzz/fragments/MovieFragment.java
@@ -3,7 +3,7 @@ package com.example.android.moviebuzz.fragments;
 import android.support.v4.app.Fragment;
 
 /**
- * Created by purus on 11/24/2017.
+ * Created by purus on 12/10/2017.
  */
 
 public class MovieFragment extends Fragment {
